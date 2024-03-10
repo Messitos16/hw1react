1f8b3318a139171d6dcb4670a8c91b0e992c7631
Messitos16 <149245706+Messitos16@users.noreply.github.com>
2024-03-10 20:41:21 +0600

    Add files via upload

---
+++ src/component/Header.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header() {
+  return (
+    <div className="Header">
+<nav>
+<div>logo</div>
+<ul>
+    <li>hello</li>
+    <li>hello</li>
+    <li>hello</li>
+    <li>hello</li>
+    <li>hello</li>
+    </ul>  
+</nav>
+    </div>
+  );
+}
+
+export default Header;
